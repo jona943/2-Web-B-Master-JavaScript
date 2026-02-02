190f466b83c5f427cbc3ce6cdaf29ee45a8329e6
Jonathan <jonathan603015@gmail.com>
2026-02-02 11:44:12 -0700

    Se implemento la impresion de calificacion

--- mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
+++ mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
@@ -1,11 +1,15 @@
-let notaAlumno = 59;
+let notaAlumno = 67;
 
 if (notaAlumno >= 90){
-    console.log("Exelente..!");
+    console.log("Su calificacion es: " + notaAlumno);
+    console.log("Excelente..!");
 } else if (notaAlumno >= 75 && notaAlumno <= 89){
+    console.log("Su calificacion es: " + notaAlumno);
     console.log("Bien");
 } else if (notaAlumno >= 60 && notaAlumno <= 74) {
+    console.log("Su calificacion es: " + notaAlumno);
     console.log("Suficiente");
 } else {
+    console.log("Su calificacion es: " + notaAlumno);
     console.log("Reprobado")
 }
