80fcb58d01ad902114fa91f0b8e11f19526aad9e
Jonathan <jonathan603015@gmail.com>
2026-02-02 12:19:11 -0700

    Integracion de validacion de nota mayor a 0 y menor que 101

--- mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
+++ mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
@@ -1,11 +1,19 @@
-let notaAlumno = 67;
+// Variable declara con un valor asignado 
+let notaAlumno = 76;
 
-if (notaAlumno >= 90){
-    console.log("Su calificacion es: " + notaAlumno + " - Excelente");
-} else if (notaAlumno >= 75 && notaAlumno <= 89){
-    console.log("Su calificacion es: " + notaAlumno + " - Bien");
-} else if (notaAlumno >= 60 && notaAlumno <= 74) {
-    console.log("Su calificacion es: " + notaAlumno + " - Suficiente");
+// Primer validacion para verificar si la nota es valida
+if (notaAlumno > 0 && notaAlumno < 101) {
+    // Evaluacion de nota con las condiciones planteadas
+    if (notaAlumno >= 90) {
+        console.log("Su calificacion es: " + notaAlumno + " - Excelente");
+    } else if (notaAlumno >= 75 && notaAlumno <= 89) {
+        console.log("Su calificacion es: " + notaAlumno + " - Bien");
+    } else if (notaAlumno >= 60 && notaAlumno <= 74) {
+        console.log("Su calificacion es: " + notaAlumno + " - Suficiente");
+    } else {
+        console.log("Su calificacion es: " + notaAlumno + " - Reprobado");
+    }
 } else {
-    console.log("Su calificacion es: " + notaAlumno + " - Reprobado");
+    // Mesaje en caso de que la nota no sea valida
+    console.log("Por favor introduce una nota valida (mayor a 0 y menor que 101).")
 }
