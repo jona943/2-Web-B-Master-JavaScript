64667ed631d71bacda0f8a9248a4669ab8a49de3
Jonathan <jonathan603015@gmail.com>
2026-02-02 09:57:50 -0700

    Agrege carpeta de entregable

---
+++ mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
@@ -0,0 +1,9 @@
+/*
+Crea un programa en JavaScript que evalúe la nota de un estudiante y genere un mensaje personalizado basado en 
+la nota. Las notas se asignan de la siguiente manera:
+
+Si la nota es 90 o más, el estudiante aprueba con "Excelente".
+Si la nota está entre 75 y 89, el estudiante aprueba con "Bien".
+Si la nota está entre 60 y 74, el estudiante aprueba con "Suficiente".
+Si la nota es menor de 60, el estudiante no aprueba.
+*/
