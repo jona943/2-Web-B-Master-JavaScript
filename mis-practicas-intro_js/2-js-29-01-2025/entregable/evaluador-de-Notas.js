e50a040377159ceafc676e85f983884ff6bb5851
Jonathan <jonathan603015@gmail.com>
2026-02-02 11:51:19 -0700

    Implementacion de concatenacion en la impresion de mensaje

--- mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
+++ mis-practicas-intro_js/2-js-29-01-2025/entregable/evaluador-de-Notas.js
@@ -1,15 +1,11 @@
 let notaAlumno = 67;
 
 if (notaAlumno >= 90){
-    console.log("Su calificacion es: " + notaAlumno);
-    console.log("Excelente..!");
+    console.log("Su calificacion es: " + notaAlumno + " - Excelente");
 } else if (notaAlumno >= 75 && notaAlumno <= 89){
-    console.log("Su calificacion es: " + notaAlumno);
-    console.log("Bien");
+    console.log("Su calificacion es: " + notaAlumno + " - Bien");
 } else if (notaAlumno >= 60 && notaAlumno <= 74) {
-    console.log("Su calificacion es: " + notaAlumno);
-    console.log("Suficiente");
+    console.log("Su calificacion es: " + notaAlumno + " - Suficiente");
 } else {
-    console.log("Su calificacion es: " + notaAlumno);
-    console.log("Reprobado")
+    console.log("Su calificacion es: " + notaAlumno + " - Reprobado");
 }
