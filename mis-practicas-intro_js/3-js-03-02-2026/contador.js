1a65af8e76d1a7ae910c296b315a703abba7ca47
Jonathan Medina <jonathan603015@gmail.com>
2026-02-03 18:40:46 -0700

    Arrays

---
+++ mis-practicas-intro_js/3-js-03-02-2026/contador.js
@@ -0,0 +1,7 @@
+let contador = 0;
+console.log("Contador utra-rapido..!");
+
+while (contador < 11){
+    console.log("Tu contador es de " + contador);
+    contador ++;
+}
--- mis-practicas-intro_js/3-js-03-02-2026/main.js
+++ mis-practicas-intro_js/3-js-03-02-2026/main.js
@@ -1,7 +1,25 @@
-let contador = 0;
-console.log("Contador utra-rapido..!");
+let i = 1;
 
-while (contador < 11){
-    console.log("Tu contador es de " + contador);
-    contador ++;
+while (i <= 10){
+    console.log("while: ", i);
+    i ++ 
 }
+
+let entrada = "";
+
+
+
+console.log('Saliste ...!')
+console.log("---------------------");
+
+// Arrays
+
+let frutas = ['manzanas', 'pera', 'uva', 'melon', 'papaya', 'mango'];
+
+console.log(frutas[2]);
+
+console.log(frutas.length);
+
+for (let indice = 0; indice < frutas.length; indice++ ){
+    console.log("Fruta - ", frutas[indice]);
+};
