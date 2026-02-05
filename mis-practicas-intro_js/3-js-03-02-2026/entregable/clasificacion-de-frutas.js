5057b1cdbcdd3e92c78ee386d2558d5fb748b083
Jonathan <jonathan603015@gmail.com>
2026-02-05 15:17:57 -0700

    Inicio entregable

---
+++ mis-practicas-intro_js/3-js-03-02-2026/entregable/clasificacion-de-frutas.js
@@ -0,0 +1,7 @@
+let frutas = ["manzana", "plátano", "naranja", "fresa", "uva", "piña", "mango", "sandía", "kiwi", "pera", "cereza", "limón"];
+
+let tipo_fruta = ["Pomos", "Cítricos", "Drupas", "Bayas", "Infrutescencia"];
+
+for (let indice = 0; indice < frutas.length; indice++){
+    console.log(indice + ". " + frutas[indice]);
+};
