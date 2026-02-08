d0066135179b624e9aa6190b02553f87908654a1
Jonathan <jonathan603015@gmail.com>
2026-02-08 11:58:40 -0700

    Practica

--- mis-practicas-intro_js/4-js-05-02-2026/main.js
+++ mis-practicas-intro_js/4-js-05-02-2026/main.js
@@ -35,3 +35,62 @@ console.log('-----------------------------------');
 
 let resultado; // Variable global 
 console.log(resultado); // Undefined
+
+function sinReturn(){
+    let x = 5;
+}
+// Se ejecuta la funcion pero NO regresa nada 
+console.log(sinReturn()) // undefined
+
+function multiplicar(dato1, dato2){
+    return dato1 * dato2;
+}
+
+console.log(multiplicar(4)) // NaN
+// Devuelve Not A Number (NaN), hace falta un argumento
+
+console.log('-----------------------------------');
+
+// Arrow function - Funcion de flecha 
+console.log("-------------Funcion con flecha------------->")
+
+const dividir = (a, b) => a/b; // return automatico 
+
+console.log(dividir(36,4));
+
+const suma = (a,b) => { // ----->
+    // Una funcion mas larga con {} no tiene return automatico
+    return a + b;
+}
+
+console.log(suma(5, 8));
+
+const multi = (a,b) => {return a * b}
+
+console.log(multi(85,6));
+
+console.log('-----------------------------------');
+console.log("Funciones anonimas");
+
+// No es necesario darles un nombre ya que ejecunta algo puntual
+// Callbacks
+
+setTimeout(function () {
+    console.log("Este es un mensaje que se ejecuta despues....")
+    
+}, 5000);
+
+// CUANDO NO UTILIZAR FUNCIONES ANONIMAS 
+/*Se tiene que reutilizar muchas veces 
+Tiene logica grande
+Necesita ser testeada
+Se necesita claridad */
+
+
+function mascotas(a,b,c){
+    console.log("Nombres de las mascotas: ",  a + b + c)
+}
+
+mascotas('Puppy ', 'Bombon '); // El elemento c esta indefinido
+
+
