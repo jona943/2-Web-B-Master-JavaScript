c9b3bbc5760b38981fbc5900796d7d4a1b7aee14
Jonathan Medina <jonathan603015@gmail.com>
2026-02-10 19:16:26 -0700

    objetos con JS

--- mis-practicas-intro_js/5-js-10-02-2026/objetos.js
+++ mis-practicas-intro_js/5-js-10-02-2026/objetos.js
@@ -32,6 +32,23 @@ const albumBeatles = {
     this.canciones.forEach((cancion, index) => {
       console.log(`${index + 1}. ${cancion}`);
     });
+  },
+
+  // Método para añadir una nueva canción al final del álbum.
+  agregarCancion: function(cancionNueva) {
+    this.canciones.push(cancionNueva);
+    console.log("Cancion agregada");
+  },
+
+  // Método para eliminar una canción del álbum por su nombre.
+  eliminarCancion: function(cancionAEliminar) {
+    const index = this.canciones.indexOf(cancionAEliminar);
+    if (index !== -1) {
+      this.canciones.splice(index, 1);
+      console.log("Cancion eliminada");
+    } else {
+      console.log("Cancion no encontrada");
+    }
   }
 };
 
@@ -49,3 +66,37 @@ console.log(albumBeatles.año);
 albumBeatles.mostrarInfo();
 // Llama al método listarCanciones para imprimir la lista numerada de temas.
 albumBeatles.listarCanciones();
+
+// Ejemplo de cómo usar el nuevo método agregarCancion.
+albumBeatles.agregarCancion("The Long One");
+// Vuelve a listar las canciones para ver la nueva adición.
+albumBeatles.listarCanciones();
+
+// Ejemplo de cómo usar el nuevo método eliminarCancion.
+console.log("\n--- Probando eliminarCancion ---");
+albumBeatles.eliminarCancion("Octopus's Garden"); // Elimina una existente
+// Vuelve a listar las canciones para ver la eliminación.
+albumBeatles.listarCanciones();
+
+albumBeatles.eliminarCancion("Cancion Inexistente"); // Prueba de canción no encontrada
+albumBeatles.listarCanciones(); // Para mostrar que no hubo cambios
+
+albumBeatles.eliminarCancion("Here Comes the Sun"); // Cancion eliminidada
+albumBeatles.listarCanciones();
+
+
+console.log("---------------------------");
+
+// Desestructurar - es SACAR cosas de un objeto o array SIN necesidad de escribir objeto.propiedad o array[indice]
+
+const { canciones: cancionesDelAlbum } = albumBeatles; // Desestructura el array 'canciones' del objeto 'albumBeatles'
+const [primera, segunda, tercera] = cancionesDelAlbum; // Desestructura los primeros 3 elementos del array 'cancionesDelAlbum'
+console.log(primera);
+console.log(segunda);
+console.log(tercera);
+
+// Desestructuración anidada: extrae el array 'canciones' y, de él, los primeros 3 elementos directamente.
+const { canciones: [pista1, pista2, pista3]} = albumBeatles;
+console.log(pista1);
+console.log(pista2);
+console.log(pista3);
