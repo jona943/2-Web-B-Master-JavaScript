6627926bb43b77ae7799a808e0af621cc7d59a8e
Jonathan <jonathan603015@gmail.com>
2026-02-10 18:20:17 -0700

    objetos

---
+++ mis-practicas-intro_js/5-js-10-02-2026/objetos.js
@@ -0,0 +1,27 @@
+const albumBeatles = {
+  titulo: "Abbey Road",
+  artista: "The Beatles",
+  año: 1969,
+  canciones: [
+    "Come Together",
+    "Something",
+    "Maxwell's Silver Hammer",
+    "Oh! Darling",
+    "Octopus's Garden",
+    "I Want You (She's So Heavy)",
+    "Here Comes the Sun",
+    "Because",
+    "You Never Give Me Your Money",
+    "Sun King",
+    "Mean Mr. Mustard",
+    "Polythene Pam",
+    "She Came In Through the Bathroom Window",
+    "Golden Slumbers",
+    "Carry That Weight",
+    "The End",
+    "Her Majesty"
+  ]
+};
+
+
+console.log(albumBeatles.canciones);
