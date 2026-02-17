b5a230b24145a87ed692d77eebcae59f7cddce0e
Jonathan <jonathan603015@gmail.com>
2026-02-17 16:24:23 -0700

    Solucion implementada

--- mis-practicas-intro_js/6-js-12-02-2026/entregable/caja_comentarios.js
+++ mis-practicas-intro_js/6-js-12-02-2026/entregable/caja_comentarios.js
@@ -1,16 +1,22 @@
+// Mis constantes
 const nombre = document.getElementById("name");
 const comentario = document.getElementById("comentario");
 const enviar = document.getElementById("enviar");
 const commentUser = document.getElementById("commentUser");
 
-enviar.addEventListener("click", function(event){
+// Accion del boton enviar
+enviar.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("Se clikeo el boton enviar.!!");
-    console.log(nombre.value);
+    // Obtengo los valores de los text area en una variable n - c
+    let n = nombre.value;
+    let c = comentario.value;
+    // Creo un nuevo elemento en html tipo parrafo en una constante
+    const nuevoComentario = document.createElement("p");
+    // Se asigana el texto del usuario
+    nuevoComentario.textContent = n + ": " + c;
+    // Añade el nuevo elemento a la pagimagit
+    commentUser.appendChild(nuevoComentario);
 });
 
-let n = nombre.value;
-let c = comentario.value;
 
-document.createElement("p");
 
